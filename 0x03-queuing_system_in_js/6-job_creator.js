0f1d8bf09e26de519a5dadc3d368060756794748
johnlivingprooff <johnlivingprooff@gmail.com>
2024-06-25 23:46:27 +0200

    7. Create the Job processor

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,28 @@
+import kue from 'kue';
+
+const queue = kue.createQueue();
+
+const jobData = {
+  phoneNumber: '1234567890',
+  message: 'This is a test notification message'
+};
+
+// Create a job in the 'push_notification_code' queue
+const job = queue.create('push_notification_code', jobData)
+  .save((err) => {
+    if (!err) {
+      console.log(`Notification job created: ${job.id}`);
+    } else {
+      console.log('Error creating job:', err);
+    }
+  });
+
+// Handle job completion
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+// Handle job failure
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
